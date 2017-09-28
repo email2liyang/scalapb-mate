42f0d1bdc35697388e3ee8e98d410c423671dd27
Ivan Li <ivan.li@practiceinsight.io>
2017-09-28 21:18:08 +0800

    add log

--- message-service/src/main/scala/io/datanerd/securemsg/service/MessageServiceImpl.scala
+++ message-service/src/main/scala/io/datanerd/securemsg/service/MessageServiceImpl.scala
@@ -3,15 +3,18 @@ package io.datanerd.securemsg.service
 import java.util.concurrent.Executors
 
 import io.datanerd.generated.securemsg._
+import org.slf4j.{Logger, LoggerFactory}
 
 import scala.concurrent.Future
 
 class MessageServiceImpl extends MessageServiceGrpc.MessageService {
 
+  private val log: Logger = LoggerFactory.getLogger(this.getClass)
   // see more from https://www.beyondthelines.net/computing/scala-future-and-execution-context/
   implicit val executor = scala.concurrent.ExecutionContext.fromExecutorService(Executors.newCachedThreadPool())
 
   override def postMessage(request: SecureMsg): Future[PostResult] = {
+    log.info("calling post message")
     Future.successful(PostResult().withMessageUrl("x"))
   }
 
