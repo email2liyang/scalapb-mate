4b4e28e9e2933c1e7f0b5e84408c9e3a772f9638
Ivan Li <ivan.li@practiceinsight.io>
2017-10-02 20:20:04 +0800

    use right collection

--- message-service/src/main/scala/io/datanerd/securemsg/dao/MessageDao.scala
+++ message-service/src/main/scala/io/datanerd/securemsg/dao/MessageDao.scala
@@ -2,23 +2,29 @@ package io.datanerd.securemsg.dao
 
 import com.google.inject.Inject
 import io.datanerd.securemsg.guice.MongoDbName
+import org.slf4j.{Logger, LoggerFactory}
 import reactivemongo.api.MongoConnection
 import reactivemongo.api.collections.bson.BSONCollection
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
-import org.slf4j.{Logger, LoggerFactory}
 
 class MessageDao @Inject()(connection: MongoConnection, @MongoDbName dbName: String) {
 
   val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
   def getCollection(): Future[BSONCollection] = {
-    connection.database(dbName).map(_.collection("startup_log"))
+    connection.database(dbName).map(_.collection("message"))
   }
 
   def isConnected: Boolean = {
-    Await.result(getCollection().flatMap(_.count()), Duration.Inf) > 0
+    //make sure index is available
+    Await
+      .result(
+        getCollection().flatMap(_.indexesManager.list()),
+        1.second
+      )
+      .nonEmpty
   }
 }
