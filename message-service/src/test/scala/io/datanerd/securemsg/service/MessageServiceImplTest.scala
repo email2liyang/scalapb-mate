4b5e4f16d0ebf56c4ef4b313124103fb8f011e53
Ivan Li <ivan.li@practiceinsight.io>
2017-10-07 21:11:42 +0800

    test service method with dao

--- message-service/src/test/scala/io/datanerd/securemsg/service/MessageServiceImplTest.scala
+++ message-service/src/test/scala/io/datanerd/securemsg/service/MessageServiceImplTest.scala
@@ -8,6 +8,7 @@ import io.datanerd.generated.securemsg.{MessageId, MessageServiceGrpc, PostResul
 import io.datanerd.securemsg.ServiceSpec
 import io.datanerd.securemsg.dao.MessageDao
 import io.datanerd.securemsg.guice.SecureMessageModule
+import io.grpc.StatusRuntimeException
 import io.grpc.inprocess.{InProcessChannelBuilder, InProcessServerBuilder}
 import org.assertj.core.api.Assertions
 import org.mockito.Mockito
@@ -18,7 +19,7 @@ class MessageServiceImplTest extends ServiceSpec {
 
   val faker = new Faker()
 
-  def withParams(testCode: (MessageServiceBlockingStub,MessageDao) => Any) = {
+  def withParams(testCode: (MessageServiceBlockingStub, MessageDao) => Any) = {
     import net.codingwell.scalaguice.InjectorExtensions._
     val messageDao = mock[MessageDao]
     val injector = Guice.createInjector(
@@ -36,18 +37,28 @@ class MessageServiceImplTest extends ServiceSpec {
     val blockingStub = MessageServiceGrpc.blockingStub(channel)
     grpcServer.start()
 
-    testCode(blockingStub,messageDao)
+    testCode(blockingStub, messageDao)
 
     channel.shutdown()
     grpcServer.shutdown()
   }
 
-  "get message" should "return data" in withParams { (stub, messageDao) =>
+  "get message" should "return data when dao return data" in withParams { (stub, messageDao) =>
+    Mockito.when(messageDao.getMessage(org.mockito.ArgumentMatchers.anyString()))
+      .thenReturn(Future.successful(Some(SecureMsg().withEncryptedData("data"))))
     val secureMessageResult = stub.getMessage(new MessageId().withMessageId(faker.numerify("id-#")))
     Assertions.assertThat(secureMessageResult.encryptedData).isEqualTo("data")
   }
 
-  "postMessage" should "return x" in withParams { (stub,messageDao) =>
+  it should "report error when dao return none" in withParams { (stub, messageDao) =>
+    Mockito.when(messageDao.getMessage(org.mockito.ArgumentMatchers.anyString()))
+      .thenReturn(Future.successful(None))
+    assertThrows[StatusRuntimeException] {
+      stub.getMessage(new MessageId().withMessageId(faker.numerify("id-#")))
+    }
+  }
+
+  "postMessage" should "return x" in withParams { (stub, messageDao) =>
 
     Mockito.when(messageDao.saveSecureMsg(org.mockito.ArgumentMatchers.any()))
       .thenReturn(Future.successful(PostResult("x")))
