0bc7698a75fbb8b1affc139fefd42ea6608fb5d6
Ivan Li <ivan.li@practiceinsight.io>
2017-10-09 20:19:11 +0800

    add grpc launcher

--- message-service/src/main/scala/io/datanerd/securemsg/server/GrpcServerLauncher.scala
+++ message-service/src/main/scala/io/datanerd/securemsg/server/GrpcServerLauncher.scala
@@ -3,7 +3,7 @@ package io.datanerd.securemsg.server
 import com.google.inject.Guice
 import io.datanerd.securemsg.guice.SecureMessageModule
 
-object ServerLauncher extends App {
+object GrpcServerLauncher extends App {
 
   Guice.createInjector(new SecureMessageModule())
     .getInstance(classOf[SecureMessageServer])
